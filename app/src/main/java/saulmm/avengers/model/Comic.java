bc6e279dbe39c8df0f7d839320d9e2e5ba77dd95
Saul M <saulmm2@gmail.com>
2015-05-10 19:42:26 +0200

    Added comic fields

--- app/src/main/java/saulmm/avengers/model/Comic.java
+++ app/src/main/java/saulmm/avengers/model/Comic.java
@@ -1,8 +1,15 @@
 package saulmm.avengers.model;
 
+import java.util.List;
+
+import saulmm.avengers.model.rest.Thumbnail;
+
 public class Comic {
 
     private String id;
+    private String title;
+    private String desccription;
+    private List<Thumbnail> images;
     private int pageCount;
 
     public String getId() {
