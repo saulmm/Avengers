271c76033e5ebf6021f2bf7dc54e52b5d71214ca
Saul M <saulmm2@gmail.com>
2015-07-16 23:57:50 +0200

    Created Utils class

--- app/src/main/java/saulmm/avengers/Utils.java
+++ app/src/main/java/saulmm/avengers/Utils.java
@@ -1,6 +1,12 @@
 package saulmm.avengers;
 
+import saulmm.avengers.views.activities.AvengersListActivity;
+
 public class Utils {
     public static final int DIALOG_ACCEPT = -1;
     public static final int DIALOG_CANCEL = -2;
+
+    public static String getListTransitionName (int position) {
+        return AvengersListActivity.CHARACTER_IMG_NAME + position;
+    }
 }
