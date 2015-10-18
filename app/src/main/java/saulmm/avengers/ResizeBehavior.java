f25b5c606d7b836af9572e567915d882af7f2747
Saul M <saulmm2@gmail.com>
2015-10-18 22:01:40 +0200

    Refactored ResizeBehavior

--- app/src/main/java/saulmm/avengers/ResizeBehavior.java
+++ app/src/main/java/saulmm/avengers/ResizeBehavior.java
@@ -9,13 +9,7 @@ import android.widget.ImageView;
 
 @SuppressWarnings("unused")
 public class ResizeBehavior extends CoordinatorLayout.Behavior<ImageView> {
-
-    private final static float MIN_AVATAR_PERCENTAGE_SIZE   = 0.3f;
-    private final static int EXTRA_FINAL_AVATAR_PADDING     = 80;
-
     private final static String TAG = "behavior";
-    private final Context mContext;
-    private float mAvatarMaxSize;
 
     private float mFinalLeftAvatarPadding;
     private float mStartPosition;
@@ -24,19 +18,6 @@ public class ResizeBehavior extends CoordinatorLayout.Behavior<ImageView> {
     private int mFinalWidth;
 
     public ResizeBehavior(Context context, AttributeSet attrs) {
-        mContext = context;
-        init();
-
-        mFinalLeftAvatarPadding = context.getResources().getDimension(
-            R.dimen.spacing_normal);
-    }
-
-    private void init() {
-        bindDimensions();
-    }
-
-    private void bindDimensions() {
-        mAvatarMaxSize = mContext.getResources().getDimension(R.dimen.image_width);
     }
 
     private int finalHeight;
@@ -50,21 +31,13 @@ public class ResizeBehavior extends CoordinatorLayout.Behavior<ImageView> {
     @Override
     public boolean onDependentViewChanged(CoordinatorLayout parent, ImageView child, View dependency) {
         shouldInitProperties(child, dependency);
-
-        final int maxScrollDistance = (int) (mStartToolbarPosition - getStatusBarHeight());
-        float expandedPercentageFactor = dependency.getY() / maxScrollDistance;
+        float expandedPercentageFactor = dependency.getY() / mStartHeight;
 
         // The child will remain always in the middle of its dependency
         child.setY(((dependency.getY() /2) + (dependency.getHeight() /2)) - child.getHeight()/2);
-
         float collapsedHeight = ((mStartHeight) * (1f - Math.abs(expandedPercentageFactor)));
 
-            String log = String.format("%d * (1f - %.2f) = %d", (int) mStartHeight, Math.abs(expandedPercentageFactor), (int) collapsedHeight);
-            System.out.println("[DEBUG]" + " ResizeBehavior onDependentViewChanged - " + log);
-        //float widthToSubtract = ((mStartWidth - mFinalWidth) * (1f - expandedPercentageFactor));
-
         CoordinatorLayout.LayoutParams lp = (CoordinatorLayout.LayoutParams) child.getLayoutParams();
-        //lp.width = (int) (mStartWidth - widthToSubtract);
         if (collapsedHeight > finalHeight) lp.height = (int) collapsedHeight;
         child.setLayoutParams(lp);
         return true;
@@ -78,21 +51,9 @@ public class ResizeBehavior extends CoordinatorLayout.Behavior<ImageView> {
             mStartWidth = child.getWidth();
 
         if (finalHeight == 0) finalHeight =
-            mContext.getResources().getDimensionPixelOffset(R.dimen.image_final_height);
-
-
+            child.getContext().getResources().getDimensionPixelOffset(R.dimen.image_final_height);
 
         if (mStartToolbarPosition == 0)
             mStartToolbarPosition = dependency.getY() + (dependency.getHeight()/2);
-    }
-
-    public int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = mContext.getResources().getIdentifier("status_bar_height", "dimen", "android");
-
-        if (resourceId > 0) {
-            result = mContext.getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
     }
 }
