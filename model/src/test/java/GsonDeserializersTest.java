2965ec24a3c0658dae1f48ad08d5ed6bd8dfd2de
Saul M <saulmm2@gmail.com>
2015-12-13 17:52:18 +0100

    Added Serializers test

---
+++ model/src/test/java/GsonDeserializersTest.java
@@ -0,0 +1,35 @@
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import org.junit.Test;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+import saulmm.avengers.entities.MarvelCharacter;
+import saulmm.avengers.rest.utils.deserializers.MarvelResultsDeserializer;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+
+public class GsonDeserializersTest {
+
+    @Test public void testThatACharacterDeseralizersDeserializesACharacter() throws Exception {
+        MarvelResultsDeserializer<MarvelCharacter> marvelResultsDeserializer = new MarvelResultsDeserializer<>();
+        JsonElement characterElement = new JsonParser().parse(getCharacterJsonString());
+        Type t = new TypeToken<List<MarvelCharacter>>() {}.getType();
+
+        List<MarvelCharacter> characterList = marvelResultsDeserializer
+            .deserialize(characterElement, t, mock(JsonDeserializationContext.class));
+
+        assertThat(characterList.isEmpty(), is(false));
+        assertThat(characterList.get(0).getName(), is("3-D Man"));
+    }
+
+    private String getCharacterJsonString() {
+        return "{\"code\":200,\"status\":\"Ok\",\"copyright\":\"© 2015 MARVEL\",\"attributionText\":\"Data provided by Marvel. © 2015 MARVEL\",\"attributionHTML\":\"<a href=\\\"http://marvel.com\\\">Data provided by Marvel. © 2015 MARVEL</a>\",\"etag\":\"7e4b284d9477110570b89bc50bd1f0f51a8ba07f\",\"data\":{\"offset\":0,\"limit\":20,\"total\":1,\"count\":1,\"results\":[{\"id\":1011334,\"name\":\"3-D Man\",\"description\":\"\",\"modified\":\"2014-04-29T14:18:17-0400\",\"thumbnail\":{\"path\":\"http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784\",\"extension\":\"jpg\"},\"resourceURI\":\"http://gateway.marvel.com/v1/public/characters/1011334\",\"comics\":{\"available\":11,\"collectionURI\":\"http://gateway.marvel.com/v1/public/characters/1011334/comics\",\"items\":[{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/21366\",\"name\":\"Avengers: The Initiative (2007) #14\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/24571\",\"name\":\"Avengers: The Initiative (2007) #14 (SPOTLIGHT VARIANT)\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/21546\",\"name\":\"Avengers: The Initiative (2007) #15\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/21741\",\"name\":\"Avengers: The Initiative (2007) #16\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/21975\",\"name\":\"Avengers: The Initiative (2007) #17\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/22299\",\"name\":\"Avengers: The Initiative (2007) #18\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/22300\",\"name\":\"Avengers: The Initiative (2007) #18 (ZOMBIE VARIANT)\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/22506\",\"name\":\"Avengers: The Initiative (2007) #19\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/10223\",\"name\":\"Marvel Premiere (1972) #35\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/10224\",\"name\":\"Marvel Premiere (1972) #36\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/comics/10225\",\"name\":\"Marvel Premiere (1972) #37\"}],\"returned\":11},\"series\":{\"available\":2,\"collectionURI\":\"http://gateway.marvel.com/v1/public/characters/1011334/series\",\"items\":[{\"resourceURI\":\"http://gateway.marvel.com/v1/public/series/1945\",\"name\":\"Avengers: The Initiative (2007 - 2010)\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/series/2045\",\"name\":\"Marvel Premiere (1972 - 1981)\"}],\"returned\":2},\"stories\":{\"available\":17,\"collectionURI\":\"http://gateway.marvel.com/v1/public/characters/1011334/stories\",\"items\":[{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/19947\",\"name\":\"Cover #19947\",\"type\":\"cover\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/19948\",\"name\":\"The 3-D Man!\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/19949\",\"name\":\"Cover #19949\",\"type\":\"cover\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/19950\",\"name\":\"The Devil's Music!\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/19951\",\"name\":\"Cover #19951\",\"type\":\"cover\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/19952\",\"name\":\"Code-Name:  The Cold Warrior!\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/47185\",\"name\":\"Avengers: The Initiative (2007) #14 - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/47499\",\"name\":\"Avengers: The Initiative (2007) #15 - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/47792\",\"name\":\"Avengers: The Initiative (2007) #16\",\"type\":\"cover\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/47793\",\"name\":\"Avengers: The Initiative (2007) #16 - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/48362\",\"name\":\"Avengers: The Initiative (2007) #17 - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/49104\",\"name\":\"Avengers: The Initiative (2007) #18 - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/49106\",\"name\":\"Avengers: The Initiative (2007) #18, Zombie Variant - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/49888\",\"name\":\"Avengers: The Initiative (2007) #19\",\"type\":\"cover\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/49889\",\"name\":\"Avengers: The Initiative (2007) #19 - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/54371\",\"name\":\"Avengers: The Initiative (2007) #14, Spotlight Variant - Int\",\"type\":\"interiorStory\"},{\"resourceURI\":\"http://gateway.marvel.com/v1/public/stories/96303\",\"name\":\"Deadpool (1997) #44\",\"type\":\"interiorStory\"}],\"returned\":17},\"events\":{\"available\":1,\"collectionURI\":\"http://gateway.marvel.com/v1/public/characters/1011334/events\",\"items\":[{\"resourceURI\":\"http://gateway.marvel.com/v1/public/events/269\",\"name\":\"Secret Invasion\"}],\"returned\":1},\"urls\":[{\"type\":\"detail\",\"url\":\"http://marvel.com/characters/74/3-d_man?utm_campaign=apiRef&utm_source=74129ef99c9fd5f7692608f17abb88f9\"},{\"type\":\"wiki\",\"url\":\"http://marvel.com/universe/3-D_Man_(Chandler)?utm_campaign=apiRef&utm_source=74129ef99c9fd5f7692608f17abb88f9\"},{\"type\":\"comiclink\",\"url\":\"http://marvel.com/comics/characters/1011334/3-d_man?utm_campaign=apiRef&utm_source=74129ef99c9fd5f7692608f17abb88f9\"}]}]}}";
+    }
+}
