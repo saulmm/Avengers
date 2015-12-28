7b2be6da578385ecf004fe37050ba822875b52e3
Saul M <saulmm2@gmail.com>
2015-12-28 14:22:59 +0100

    Added first espresso test

---
+++ app/src/androidTest/java/saulmm/avengers/CharacterListActivityInstrumentationTest.java
@@ -0,0 +1,41 @@
+package saulmm.avengers;
+
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import saulmm.avengers.repository.CharacterRepository;
+import saulmm.avengers.views.activities.CharacterDetailActivity;
+import saulmm.avengers.views.activities.CharacterListActivity;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.times;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CharacterListActivityInstrumentationTest {
+
+    @Rule
+    public IntentsTestRule<CharacterListActivity> mCharacterListIntentRule =
+            new IntentsTestRule<>(CharacterListActivity.class);
+
+    @Test
+    public void clickOnAvenger() {
+        Espresso.onView(withId(R.id.activity_avengers_recycler))
+            .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        intended(hasComponent(CharacterDetailActivity.class.getName()));
+    }
+
+}
