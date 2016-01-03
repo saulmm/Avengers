9667b3f61cd8c5341628217933b15b22c6bbe8cf
Saul M <saulmm2@gmail.com>
2016-01-04 00:53:27 +0100

    Fixed broken test

--- app/src/androidTest/java/saulmm/avengers/tests/CharacterListActivityInstrumentationTest.java
+++ app/src/androidTest/java/saulmm/avengers/tests/CharacterListActivityInstrumentationTest.java
@@ -1,22 +1,28 @@
-package saulmm.avengers;
+package saulmm.avengers.tests;
 
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import saulmm.avengers.R;
+import saulmm.avengers.injector.components.DaggerAvengersComponent;
+import saulmm.avengers.injector.modules.ActivityModule;
 import saulmm.avengers.repository.CharacterRepository;
 import saulmm.avengers.views.activities.CharacterDetailActivity;
 import saulmm.avengers.views.activities.CharacterListActivity;
 
+import static android.support.test.espresso.Espresso.*;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.Intents.times;
@@ -31,11 +37,15 @@ public class CharacterListActivityInstrumentationTest {
     public IntentsTestRule<CharacterListActivity> mCharacterListIntentRule =
             new IntentsTestRule<>(CharacterListActivity.class);
 
+    @Before
+    public void setup() {
+    }
+
     @Test
-    public void clickOnAvenger() {
+    public void testThatAClickOnTheAvengerOPensTheDetailActivity() {
         waitForRequest();
 
-        Espresso.onView(withId(R.id.activity_avengers_recycler))
+        onView(ViewMatchers.withId(R.id.activity_avengers_recycler))
             .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         intended(hasComponent(CharacterDetailActivity.class.getCanonicalName()));
@@ -43,7 +53,7 @@ public class CharacterListActivityInstrumentationTest {
 
     public void waitForRequest() {
         try {
-            Thread.sleep(4000);
+            Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
